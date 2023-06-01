7cfa3b0e83c28ed881f2b65f92a5d714ac4b535d
mohibbs <mohib@bs-23.net>
2023-06-02 00:12:15 +0600

    added: machine learning instations and run with jupyter

---
+++ machine_learning.py
@@ -0,0 +1,16 @@
+
+# what is machine learning
+# side kick learn
+
+# library for Machine Learning
+    # Numpy: for multidimentional array
+    # Pandas: data analysis library. it provides a concept called data from. similar to excel sheet
+    # Matplotlib: createing grap and plot
+    # scikit-learn: provides common algorithms like decision trees, neural networks and so on. Now
+    # Jupyter: like VS code for machine learning code to use it easyly to inspect our data
+
+# install anaconda
+# add path
+# jupyter notebook
+# where you open Directory
+
